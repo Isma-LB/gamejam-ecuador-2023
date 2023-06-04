3363ef81cb8270bf96291b3c1636bac06177a1e0
mauro4gt <85660707+mauro4gt@users.noreply.github.com>
2023-06-04 09:16:46 -0500

    cancion menu
    menuInicial script

--- Assets/Scripts/MenuInicial.cs
+++ Assets/Scripts/MenuInicial.cs
@@ -8,6 +8,7 @@ public class MenuInicial : MonoBehaviour
     public void Jugar()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        FindObjectOfType<AudioManager>().Play("Theme");
     }
 
     public void Salir()
